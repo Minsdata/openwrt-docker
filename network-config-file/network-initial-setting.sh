ed09c12920e4cad2cfdb63b2fbe87e4cbcdc11b8
Min <66832998+Minsdata@users.noreply.github.com>
2023-09-03 12:54:08 +0900

    Update network-initial-setting.sh

--- network-config-file/network-initial-setting.sh
+++ network-config-file/network-initial-setting.sh
@@ -1,9 +1,9 @@
 sed -i "2i # network config" ./lede/package/lean/default-settings/files/zzz-default-settings # Set network config
-sed -i "3i uci set network.lan.ipaddr='192.168.1.254'"  ./lede/package/lean/default-settings/files/zzz-default-settings # Default IP address
+sed -i "3i uci set network.lan.ipaddr='172.17.1.1'"  ./lede/package/lean/default-settings/files/zzz-default-settings # Default IP address
 sed -i "4i uci set network.lan.proto='static'"  ./lede/package/lean/default-settings/files/zzz-default-settings # Static IP
 sed -i "5i uci set network.lan.type='bridge'"  ./lede/package/lean/default-settings/files/zzz-default-settings  # Interface Type: Bridge
 sed -i "6i uci set network.lan.ifname='eth0'"  ./lede/package/lean/default-settings/files/zzz-default-settings  # Network port: default eth0, the first interface
 sed -i "7i uci set network.lan.netmask='255.255.0.0'"  ./lede/package/lean/default-settings/files/zzz-default-settings    # subnet mask
-sed -i "8i uci set network.lan.gateway='192.168.1.1'"  ./lede/package/lean/default-settings/files/zzz-default-settings  # Default gateway address (main route IP)
-sed -i "9i uci set network.lan.dns='192.168.1.1'"  ./lede/package/lean/default-settings/files/zzz-default-settings  # Default upstream DNS address
+sed -i "8i uci set network.lan.gateway='172.17.0.1'"  ./lede/package/lean/default-settings/files/zzz-default-settings  # Default gateway address (main route IP)
+sed -i "9i uci set network.lan.dns='1.1.1.1'"  ./lede/package/lean/default-settings/files/zzz-default-settings  # Default upstream DNS address
 sed -i "10i uci commit network\n"  ./lede/package/lean/default-settings/files/zzz-default-settings
